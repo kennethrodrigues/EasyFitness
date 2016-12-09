4e448e26e6241bc38c63cbcc57e82af735669653
331182 <331182@DGF31165.grupofolha.intranet>
2016-12-09 19:41:33 -0200

    primeira parte

---
+++ Classes.php
@@ -0,0 +1,173 @@
+<?php
+
+abstract class Pessoa{
+
+private  $nome;
+private $rg;
+private $cpf;
+private $telefone;
+private $dtNascimento;
+private $celular;
+private $email;
+private $senha;
+private $cep;
+private $rua;
+private $numero;
+private $complemento;
+private $bairro;
+private $pais;
+private $cidade;
+private $status;
+	public function getNome() {
+		return $this->nome;
+	}
+	public function setNome($nome) {
+		$this->nome = $nome;
+		return $this;
+	}
+	public function getRg() {
+		return $this->rg;
+	}
+	public function setRg($rg) {
+		$this->rg = $rg;
+		return $this;
+	}
+	public function getCpf() {
+		return $this->cpf;
+	}
+	public function setCpf($cpf) {
+		$this->cpf = $cpf;
+		return $this;
+	}
+	public function getTelefone() {
+		return $this->telefone;
+	}
+	public function setTelefone($telefone) {
+		$this->telefone = $telefone;
+		return $this;
+	}
+	public function getDtNascimento() {
+		return $this->dtNascimento;
+	}
+	public function setDtNascimento($dtNascimento) {
+		$this->dtNascimento = $dtNascimento;
+		return $this;
+	}
+	public function getCelular() {
+		return $this->celular;
+	}
+	public function setCelular($celular) {
+		$this->celular = $celular;
+		return $this;
+	}
+	public function getEmail() {
+		return $this->email;
+	}
+	public function setEmail($email) {
+		$this->email = $email;
+		return $this;
+	}
+	public function getSenha() {
+		return $this->senha;
+	}
+	public function setSenha($senha) {
+		$this->senha = $senha;
+		return $this;
+	}
+	public function getCep() {
+		return $this->cep;
+	}
+	public function setCep($cep) {
+		$this->cep = $cep;
+		return $this;
+	}
+	public function getRua() {
+		return $this->rua;
+	}
+	public function setRua($rua) {
+		$this->rua = $rua;
+		return $this;
+	}
+	public function getNumero() {
+		return $this->numero;
+	}
+	public function setNumero($numero) {
+		$this->numero = $numero;
+		return $this;
+	}
+	public function getComplemento() {
+		return $this->complemento;
+	}
+	public function setComplemento($complemento) {
+		$this->complemento = $complemento;
+		return $this;
+	}
+	public function getBairro() {
+		return $this->bairro;
+	}
+	public function setBairro($bairro) {
+		$this->bairro = $bairro;
+		return $this;
+	}
+	public function getPais() {
+		return $this->pais;
+	}
+	public function setPais($pais) {
+		$this->pais = $pais;
+		return $this;
+	}
+	public function getCidade() {
+		return $this->cidade;
+	}
+	public function setCidade($cidade) {
+		$this->cidade = $cidade;
+		return $this;
+	}
+	public function getStatus() {
+		return $this->status;
+	}
+	public function setStatus($status) {
+		$this->status = $status;
+		return $this;
+	}
+		
+}
+
+class Aluno extends Pessoa{
+	private $matricula;
+	private $plano;
+	public function getMatricula() {
+		return $this->matricula;
+	}
+	public function setMatricula($matricula) {
+		$this->matricula = $matricula;
+		return $this;
+	}
+	public function getPlano() {
+		return $this->plano;
+	}
+	public function setPlano($plano) {
+		$this->plano = $plano;
+		return $this;
+	}
+	
+	
+	
+}
+
+class Funcionario extends Pessoa{
+	private $tipo;
+	
+	public function getTipo() {
+		return $this->tipo;
+	}
+	public function setTipo($tipo) {
+		$this->tipo = $tipo;
+		return $this;
+	}
+		
+}
+
+
+
+?>
