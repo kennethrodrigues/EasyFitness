0acac6e73011239c315f9062d891cb929d6ba276
magnilson Teixeira <magnilson_silva@hotmail.com>
2016-12-09 20:20:58 -0200

    class Funcionario extends Pessoa { 	private $tipo; 	public function getTipo() { 		return $this->tipo; 	} 	public function setTipo($tipo) { 		$this->tipo = $tipo; 		return $this; 	} }

--- Classes.php
+++ Classes.php
@@ -129,16 +129,4 @@ abstract class Pessoa {
 		return $this;
 	}
 }
-
-class Funcionario extends Pessoa {
-	private $tipo;
-	public function getTipo() {
-		return $this->tipo;
-	}
-	public function setTipo($tipo) {
-		$this->tipo = $tipo;
-		return $this;
-	}
-}
-
 ?>
